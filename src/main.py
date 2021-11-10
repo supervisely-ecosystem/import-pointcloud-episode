f4c3bd4c93b099cb4a51225bf064a232f984313d
kashkarov <kashkarov@dedicorp.ru>
2021-11-10 15:05:55 +0300

    api methods changed

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 import supervisely_lib as sly
 from supervisely_lib.api.module_api import ApiField
-from supervisely_lib.api.pointcloud.pointcloud_episode_annotation_api import PointcloudEpisodeAnnotationAPI
 from supervisely_lib.pointcloud_annotation.pointcloud_episode_annotation import PointcloudEpisodeAnnotation
 from supervisely_lib.project.pointcloud_episode_project import PointcloudEpisodeProject
 from supervisely_lib.project.project_type import ProjectType
@@ -23,13 +22,13 @@ def upload_related_items(api, related_items, pointcloud_id):
     if len(related_items) != 0:
         img_infos = []
         for img_path, meta_json in related_items:
-            img = api.pointcloud.upload_related_image(img_path)[0]
+            img = api.pointcloud_episode.upload_related_image(img_path)[0]
             img_infos.append({ApiField.ENTITY_ID: pointcloud_id,
                                ApiField.NAME: meta_json[ApiField.NAME],
                                ApiField.HASH: img,
                                ApiField.META: meta_json[ApiField.META]})
 
-        api.pointcloud.add_related_images(img_infos)
+        api.pointcloud_episode.add_related_images(img_infos)
 
 
 def process_episode_project(input_dir, project, api, app_logger):
@@ -37,8 +36,6 @@ def process_episode_project(input_dir, project, api, app_logger):
 
     api.project.update_meta(project.id, project_fs.meta.to_json())
     app_logger.info("Project {!r} [id={!r}] has been created".format(project.name, project.id))
-
-    episode_annotation_api = PointcloudEpisodeAnnotationAPI(api)
 
     uploaded_objects = KeyIdMap()
     for dataset_fs in project_fs:
@@ -64,13 +61,13 @@ def process_episode_project(input_dir, project, api, app_logger):
             except (KeyError, IndexError):
                 item_meta = {}
 
-            pointcloud = api.pointcloud.upload_path(dataset.id, item_name, item_path, item_meta)  # upload pointcloud
+            pointcloud = api.pointcloud_episode.upload_path(dataset.id, item_name, item_path, item_meta)  # upload pointcloud
             upload_related_items(api, related_items, pointcloud.id)  # upload related_images if exist
 
             frame_idx = dataset_fs.get_frame_idx(item_name)
             frame_to_pointcloud_ids[frame_idx] = pointcloud.id
 
-        episode_annotation_api.append(dataset.id, episode_annotation, frame_to_pointcloud_ids, uploaded_objects)
+        api.pointcloud_episode.annotation.append(dataset.id, episode_annotation, frame_to_pointcloud_ids, uploaded_objects)
 
     app_logger.info('PROJECT_UPLOADED', extra={'event_type': sly.EventType.PROJECT_CREATED, 'project_id': project.id})
 

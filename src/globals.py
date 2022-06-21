ebb4761aed5d3145ed88eabba18aba9684392657
Daniel Y <33942379+qanelph@users.noreply.github.com>
2022-06-21 20:35:36 +0300

    Update globals.py

--- src/globals.py
+++ src/globals.py
@@ -11,6 +11,9 @@ TEAM_ID = os.environ["context.teamId"]
 WORKSPACE_ID = os.environ["context.workspaceId"]
 INPUT_DIR = os.environ.get("modal.state.slyFolder")
 INPUT_FILE = os.environ.get("modal.state.slyFile")
+
+OUTPUT_PROJECT_NAME = os.environ.get("modal.state.project_name", "")
+REMOVE_SOURCE = bool(strtobool(os.getenv("modal.state.remove_source")))
 assert INPUT_DIR or INPUT_FILE
 
 storage_dir = my_app.data_dir

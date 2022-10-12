c9730536b73a1f9d4fd05c6e4316c38d47184833
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2022-10-12 14:57:43 +0400

    add import from agent support (#4)
    * add import from agent support
    
    * add import from agent support

--- src/globals.py
+++ src/globals.py
@@ -1,10 +1,18 @@
 import os
+from pathlib import Path
 import supervisely as sly
-from supervisely_lib.app.v1.app_service import AppService
+from supervisely.app.v1.app_service import AppService
 
 
 from distutils.util import strtobool
 
+app_root_directory = str(Path(__file__).parent.absolute().parents[0])
+sly.logger.info(f"App root directory: {app_root_directory}")
+
+# debug
+# from dotenv import load_dotenv
+# load_dotenv(os.path.join(app_root_directory, "debug.env"))
+# load_dotenv(os.path.join(app_root_directory, "secret_debug.env"))
 
 api: sly.Api = sly.Api.from_env()
 my_app = AppService()
@@ -18,6 +26,11 @@ INPUT_FILE = os.environ.get("modal.state.slyFile")
 OUTPUT_PROJECT_NAME = os.environ.get("modal.state.project_name", "")
 REMOVE_SOURCE = bool(strtobool(os.getenv("modal.state.remove_source")))
 assert INPUT_DIR or INPUT_FILE
+
+if INPUT_DIR:
+    IS_ON_AGENT = api.file.is_on_agent(INPUT_DIR)
+else:
+    IS_ON_AGENT = api.file.is_on_agent(INPUT_FILE)
 
 storage_dir = my_app.data_dir
 sly.fs.mkdir(storage_dir, True)

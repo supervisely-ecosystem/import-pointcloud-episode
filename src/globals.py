40d756c0fd9d56f694a57b80d6e7ee0732c97e4c
peixed351 <peixed351@gmail.com>
2023-02-06 12:35:03 +0300

    add import template

--- src/globals.py
+++ src/globals.py
@@ -1,23 +1,24 @@
 import os
-from pathlib import Path
+
+# from pathlib import Path
 import supervisely as sly
-from supervisely.app.v1.app_service import AppService
+
+# from supervisely.app.v1.app_service import AppService
 from distutils.util import strtobool
 
-app_root_directory = str(Path(__file__).parent.absolute().parents[0])
-sly.logger.info(f"App root directory: {app_root_directory}")
+# app_root_directory = str(Path(__file__).parent.absolute().parents[0])
+# sly.logger.info(f"App root directory: {app_root_directory}")
+from dotenv import load_dotenv
 
-# debug
-# from dotenv import load_dotenv
-# load_dotenv(os.path.join(app_root_directory, "debug.env"))
-# load_dotenv(os.path.join(app_root_directory, "secret_debug.env"))
+load_dotenv("local.env")
+load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 api: sly.Api = sly.Api.from_env()
-my_app = AppService()
+# my_app = AppService()
 
-TASK_ID = int(os.environ["TASK_ID"])
-TEAM_ID = os.environ["context.teamId"]
-WORKSPACE_ID = os.environ["context.workspaceId"]
+# TASK_ID = int(os.environ["TASK_ID"])
+# TEAM_ID = os.environ["context.teamId"]
+# WORKSPACE_ID = os.environ["context.workspaceId"]
 INPUT_DIR = os.environ.get("modal.state.slyFolder")
 INPUT_FILE = os.environ.get("modal.state.slyFile")
 
@@ -30,5 +31,5 @@ if INPUT_DIR:
 else:
     IS_ON_AGENT = api.file.is_on_agent(INPUT_FILE)
 
-storage_dir = my_app.data_dir
-sly.fs.mkdir(storage_dir, True)
+STORAGE_DIR: str = sly.app.get_data_dir()
+sly.fs.mkdir(STORAGE_DIR, True)
--- src/main.py
+++ src/main.py
@@ -1,39 +1,48 @@
 import globals as g
-import functions as f
+
+# import functions as f
 import supervisely as sly
+import os, shutil
+from supervisely.io.fs import silent_remove, get_file_name
 from supervisely.project.pointcloud_episode_project import upload_pointcloud_episode_project
+from os.path import join
 
 
-@g.my_app.callback("import_pointcloud_episode")
-@sly.timeit
-def import_pointcloud_episode(api: sly.Api, task_id, context, state, app_logger):
-    input_dir, project_name = f.download_input_files(api, task_id, g.INPUT_DIR, g.INPUT_FILE)
-    
-    project_name = project_name if len(g.OUTPUT_PROJECT_NAME) == 0 else g.OUTPUT_PROJECT_NAME
+class MyImport(sly.app.Import):
+    def process(self, context: sly.app.Import.Context):
 
-    project_id, project_name = upload_pointcloud_episode_project(input_dir, api, g.WORKSPACE_ID, project_name=project_name, log_progress=True)
-    api.task.set_output_project(task_id, project_id, project_name)
-    
-    if g.REMOVE_SOURCE and not g.IS_ON_AGENT:
-        api.file.remove(team_id=g.TEAM_ID, path=g.INPUT_DIR)
-        source_dir_name = g.INPUT_DIR.strip("/")
-        sly.logger.info(
-            msg=f"Source directory: '{source_dir_name}' was successfully removed."
+        project_dir = context.path
+
+        if context.is_directory is False:
+            project_folder = join(g.STORAGE_DIR, get_file_name(project_dir))
+            shutil.unpack_archive(project_dir, project_folder)
+            silent_remove(project_dir)
+            project_dir = project_folder
+
+        project_name = (
+            os.path.basename(project_dir)
+            if len(g.OUTPUT_PROJECT_NAME) == 0
+            else g.OUTPUT_PROJECT_NAME
         )
-    
-    g.my_app.stop()
+
+        # input_dir, project_name = f.download_input_files(api, task_id, g.INPUT_DIR, g.INPUT_FILE)
+
+        # project_name = project_name if len(g.OUTPUT_PROJECT_NAME) == 0 else g.OUTPUT_PROJECT_NAME
+
+        project_id, project_name = upload_pointcloud_episode_project(
+            project_name, g.api, context.workspace_id, project_name=project_name, log_progress=True
+        )
+        # api.task.set_output_project(task_id, project_id, project_name)
+
+        if g.REMOVE_SOURCE and not g.IS_ON_AGENT:
+            if g.INPUT_DIR is not None:
+                path_to_remove = g.INPUT_DIR
+            else:
+                path_to_remove = g.INPUT_FILE
+            g.api.file.remove(team_id=context.team_id, path=path_to_remove)
+            source_dir_name = path_to_remove.strip("/")
+            sly.logger.info(msg=f"Source directory: '{source_dir_name}' was successfully removed.")
 
 
-def main():
-    sly.logger.info("Script arguments", extra={
-        "context.teamId": g.TEAM_ID,
-        "context.workspaceId": g.WORKSPACE_ID,
-        "modal.state.slyFolder": g.INPUT_DIR,
-        "modal.state.slyFile": g.INPUT_FILE,
-    })
-
-    g.my_app.run(initial_events=[{"command": "import_pointcloud_episode"}])
-
-
-if __name__ == "__main__":
-    sly.main_wrapper("main", main)
+app = MyImport()
+app.run()

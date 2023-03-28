7570a8affe687569e4f8ae0056aea8143e2aa78e
peixed351 <peixed351@gmail.com>
2023-03-28 16:54:03 +0300

    bug_fix_import_error

--- src/globals.py
+++ src/globals.py
@@ -1,21 +1,18 @@
 import os
 from pathlib import Path
 import supervisely as sly
-from supervisely.app.v1.app_service import AppService
 from distutils.util import strtobool
 
-app_root_directory = str(Path(__file__).parent.absolute().parents[0])
-sly.logger.info(f"App root directory: {app_root_directory}")
 
-# debug
-# from dotenv import load_dotenv
-# load_dotenv(os.path.join(app_root_directory, "debug.env"))
-# load_dotenv(os.path.join(app_root_directory, "secret_debug.env"))
+from dotenv import load_dotenv
+
+
+load_dotenv("local.env")
+load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 api: sly.Api = sly.Api.from_env()
-my_app = AppService()
 
-TASK_ID = int(os.environ["TASK_ID"])
+# task_id = int(os.environ["TASK_ID"])
 TEAM_ID = os.environ["context.teamId"]
 WORKSPACE_ID = os.environ["context.workspaceId"]
 INPUT_DIR = os.environ.get("modal.state.slyFolder")
@@ -30,5 +27,5 @@ if INPUT_DIR:
 else:
     IS_ON_AGENT = api.file.is_on_agent(INPUT_FILE)
 
-storage_dir = my_app.data_dir
+storage_dir = sly.app.get_data_dir()
 sly.fs.mkdir(storage_dir, True)

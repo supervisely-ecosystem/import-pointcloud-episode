d1183d5dfd42a6d187e2e4b4b823fa52c900a2b2
Almaz <79905215+almazgimaev@users.noreply.github.com>
2023-10-19 20:12:12 +0200

    Update app - add validations, logs, docker image (#13)
    * rename env
    * update gitignore
    * remove junks and search project dirs
    * handle import cases, add common error messages
    * Validate related images and annotation files
    * docker image to supervisely/import-export:6.72.160
    * fix project name function
    * add info logs
    * update info log

--- src/functions.py
+++ src/functions.py
@@ -10,20 +10,20 @@ import supervisely as sly
 
 def get_project_name_from_input_path(input_path: str) -> str:
     """Returns project name from target sly folder name."""
-    full_path_dir = os.path.dirname(input_path)
-    return os.path.basename(full_path_dir)
+    return os.path.basename(os.path.normpath(input_path))
 
 
-def download_input_files(api, task_id, input_dir, input_file):
+def download_input_files(api: sly.Api, task_id, input_dir, input_file):
     if input_dir:
         if g.IS_ON_AGENT:
             agent_id, cur_files_path = api.file.parse_agent_id_and_path(input_dir)
         else:
             cur_files_path = input_dir
 
+        if not cur_files_path.endswith("/"):
+            cur_files_path += "/"
         sizeb = api.file.get_directory_size(g.TEAM_ID, input_dir)
         extract_dir = os.path.join(g.storage_dir, cur_files_path.strip("/"))
-        project_name = Path(cur_files_path).name
         progress_cb = download_progress.get_progress_cb(api, task_id, f"Downloading {g.INPUT_DIR.strip('/')}", sizeb,
                                                         is_size=True)
         api.file.download_directory(g.TEAM_ID, input_dir, extract_dir, progress_cb)
@@ -36,7 +36,6 @@ def download_input_files(api, task_id, input_dir, input_file):
         sizeb = api.file.get_info_by_path(g.TEAM_ID, input_file).sizeb
         archive_path = os.path.join(g.storage_dir, sly.fs.get_file_name_with_ext(cur_files_path))
         extract_dir = os.path.join(g.storage_dir, sly.fs.get_file_name(cur_files_path))
-        project_name = sly.fs.get_file_name(input_file)
         progress_cb = download_progress.get_progress_cb(api, task_id, f"Downloading {g.INPUT_FILE.lstrip('/')}",
                                                         sizeb,
                                                         is_size=True)
@@ -51,4 +50,15 @@ def download_input_files(api, task_id, input_dir, input_file):
         else:
             raise NotImplementedError("File extension is not supported.")
         sly.fs.silent_remove(archive_path)
-    return extract_dir, project_name
+    sly.fs.remove_junk_from_dir(extract_dir)
+
+    def search_projects(dir_path):
+        files = os.listdir(dir_path)
+        meta_exists = "meta.json" in files
+        datasets = [f for f in files if sly.fs.dir_exists(os.path.join(dir_path, f))]
+        datasets_exists = len(datasets) > 0
+        return meta_exists and datasets_exists
+
+
+    project_dirs = [project_dir for project_dir in sly.fs.dirs_filter(extract_dir, search_projects)]
+    return project_dirs
